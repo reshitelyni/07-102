404c8d51919882271d4927f1e44992ba76e3fa73
6607changchun <luobojiaozi@163.com>
2023-09-10 15:39:46 +0800

    refactor: implement PartialEq automatically

--- src/record.rs
+++ src/record.rs
@@ -4,14 +4,14 @@
 *   Author        : 6607changchun
 *   Email         : luobojiaozi@163.com
 *   File Name     : record.rs
-*   Last Modified : 2023-09-10 13:17
+*   Last Modified : 2023-09-10 15:38
 *   Describe      : Record struct type.
 *
 * ====================================================*/
 use std::cmp::Eq;
 use std::cmp::PartialEq;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct SongRank{
     pub name: String,
     pub pack: String,
@@ -27,12 +27,6 @@ pub struct User{
     pub ptt: f32
 }
 
-impl PartialEq for SongRank{
-    fn eq(&self, other: &Self) -> bool{
-        (&self.name, &self.pack, &self.level, self.constant, self.best)
-            == (&other.name, &other.pack, &other.level, other.constant, other.best)
-    }
-}
 impl Eq for SongRank{}
 
 impl Eq for User{}
